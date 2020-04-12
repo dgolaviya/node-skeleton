09e9ea981fde145fa6cb59d59d1f7e092d4a4280
kashyap88 <kashyap.thakkar2120@gmail.com>
2020-04-13 00:22:06 +0530

    Fix issue for AWS

--- app/config/mongoose-config.js
+++ app/config/mongoose-config.js
@@ -8,14 +8,14 @@ dotenv.config();
 
 const DB_OPTIONS = {
   autoIndex: process.env.DB_AUTO_INDEX, // Don't build indexes
-  reconnectTries: process.env.DB_RECONNECT_TRIES, // Never stop trying to reconnect
-  reconnectInterval: process.env.DB_RECONNECT_INTERVAL, // Reconnect every 500ms
+  //reconnectTries: process.env.DB_RECONNECT_TRIES, // Never stop trying to reconnect
+  //reconnectInterval: process.env.DB_RECONNECT_INTERVAL, // Reconnect every 500ms
   poolSize: process.env.DB_POOLSIZE,
   minSize: process.env.DB_MIN_SIZE, //f present, the connection pool will be initialized with minSize connections, and will never dip below minSize connections
   socketTimeoutMS: process.env.DB_SOCKET_TIMEOUT_MS,
   loggerLevel: process.env.DB_LOGGER_LEVEL,
   keepAlive: process.env.DB_KEEP_ALIVE,
-  bufferMaxEntries: process.env.DB_BUFFER_MAX_ENTRIES,
+  //bufferMaxEntries: process.env.DB_BUFFER_MAX_ENTRIES,
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
